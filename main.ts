288ba5e0cd96635d2581c327ab470d7b2708beab
henrywycheng <51484937+henrywycheng@users.noreply.github.com>
2019-06-26 17:25:13 +0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,24 @@
+//% weight=0 color=#3CB371 icon="\uf2b9" block="SelfBlock"
+
+namespace selfblock {
+    /* 有傳回值旳拼塊 */
+    //% blockId="addTwoNum" block="calculate total of num1 %num1|num2 %num2"
+    //% blockGap=2 weight=90 blockExternalInputs=true
+    export function addTwoNum(num1: number, num2:number): number {
+        return num1+num2
+    }
+
+    /* 無傳回值旳拼塊 */
+    //% blockId="showStringWithTime" block="show string %str|with delay time %delay"
+    //% blockGap=2 weight=80
+    export function showStringWithTime(str: string, delay:number): void {
+        basic.showString(str,delay)
+    }
+
+    /* 有傳回值旳拼塊 */
+    //% blockId="celcius2farenheit" block="convert Celcius to Farenheit num1 %num1"
+    //% blockGap=2 weight=90
+    export function celcius2farenheit(num1: number): number {
+        return num1*9/5+32
+    }
+}
